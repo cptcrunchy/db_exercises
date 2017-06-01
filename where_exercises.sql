00c2a2eb5c5aea2ffff8dd914bbf565e8b10bcda
cptCrunchy <jasong84@gmail.com>
2017-06-01 10:11:00 -0500

    Completed where_exercise

---
+++ where_exercises.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+# Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+
+# Employees whose last name starts with 'E' — 7,330 rows.
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+# Employees hired in the 90s — 135,214 rows.
+SELECT * FROM employees WHERE hire_date BETWEEN 19900101 AND 19991231;
+
+# Employees born on Christmas — 842 rows.
+SELECT * FROM employees WHERE birth_date LIKE '%12-25';
+
+# Employees with a 'q' in their last name — 1,873 rows.
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
