9201ced11066c75646a2cfba345d2f89682e2b0c
cptCrunchy <jasong84@gmail.com>
2017-06-01 10:40:32 -0500

    Completed order by exercise

---
+++ order_by_exercise.sql
@@ -0,0 +1,22 @@
+USE employees;
+
+# Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+# SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+# SELECT * FROM employees WHERE first_name='Irena' OR first_name='Vidya' OR first_name='Maya';
+SELECT * FROM employees WHERE gender = 'M' AND (first_name='Irena' OR first_name='Vidya' OR first_name='Maya') ORDER BY last_name ASC,first_name ASC;
+
+# Employees whose last name starts with 'E' — 7,330 rows.
+# SELECT * FROM employees WHERE last_name LIKE 'E%';
+# SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' ORDER BY emp_no ASC;
+
+# Employees hired in the 90s — 135,214 rows.
+SELECT * FROM employees WHERE hire_date BETWEEN 19900101 AND 19991231 AND birth_date LIKE '%12-25';
+
+# Employees born on Christmas — 842 rows.
+SELECT * FROM employees WHERE birth_date LIKE '%12-25' ORDER BY birth_date ASC, hire_date DESC;
+
+# Employees with a 'q' in their last name — 1,873 rows.
+# SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
