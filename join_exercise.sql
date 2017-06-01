174d7edd07527850789a9cb9e2c5f4b91431a7ab
cptCrunchy <jasong84@gmail.com>
2017-06-01 16:35:16 -0500

    start of db relationship exercise

---
+++ join_exercise.sql
@@ -0,0 +1,12 @@
+use join_test_db;
+
+SELECT concat(a.name,', ', a.email) AS User_info, b.name AS Role
+FROM users AS a JOIN roles AS b ON a.role_id = b.id;
+
+SELECT CONCAT(a.name,', ', a.email) AS User_info, b.name AS Role
+FROM users AS a LEFT JOIN roles AS b ON a.role_id = b.id;
+
+SELECT concat(a.name,', ', a.email) AS User_info, b.name AS Role
+FROM users AS a RIGHT JOIN roles AS b ON a.role_id = b.id;
+
+
