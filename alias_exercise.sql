df2848194a647a0ad5a7d3f62c33b618352a1b1f
cptCrunchy <jasong84@gmail.com>
2017-06-02 14:17:58 -0500

    Updated formatting of alias exercise

--- alias_exercise.sql
+++ alias_exercise.sql
@@ -1,13 +1,13 @@
 USE employees;
 
 # Return 10 employees in a result set named 'full_name' in the format of 'lastname, firstname' for each employee.
-SELECT CONCAT(first_name, ', ', last_name) AS full_name
-FROM employees LIMIT 10;
+SELECT CONCAT(em.last_name, ', ', em.first_name) full_name
+FROM employees em LIMIT 10;
 
 # Add the date of birth for each employee as 'DOB' to the query.
-SELECT CONCAT(first_name, ', ', last_name) AS full_name, birth_date AS DOB
-FROM employees LIMIT 10;
+SELECT CONCAT(em.last_name, ', ', em.first_name) full_name, birth_date DOB
+FROM employees em LIMIT 10;
 
 # Update the query to format full name to include the employee number so it is formatted as 'employee number - lastname, firstname'.
-SELECT CONCAT(emp_no,' - ',first_name, ', ', last_name) AS full_name, birth_date AS DOB
-FROM employees LIMIT 10;
+SELECT CONCAT(em.emp_no,' - ',em.last_name, ', ', em.first_name) full_name, birth_date DOB
+FROM employees em LIMIT 10;
